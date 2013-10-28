63d96627921402ea6eaf09880b8dca8e4a15dd39
Maciej Walkowiak <maciej.walkowiak@codeahead.co>
2013-10-28 23:18:23 +0100

    - added short readme - cleanup

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-junit-drools
-============
+##junit-drools##
+
+JUnit + JBoss Drools integration
+
+	import org.drools.StatefulSession;
+    import org.junit.Test;
+    import org.junit.runner.RunWith;
+    import pl.maciejwalkowiak.drools.annotations.DroolsFiles;
+    import pl.maciejwalkowiak.drools.annotations.DroolsSession;
+
+    import static org.junit.Assert.assertNotNull;
+    
+    @RunWith(DroolsJUnitRunner.class)
+    @DroolsFiles(value = "helloworld.drl", location = "/drl/")
+    public class AppTest {
+
+        @DroolsSession
+        StatefulSession session;
+
+        @Test
+        public void should_set_drools_session() {
+            assertNotNull(session);
+            session.insert(new App());
+            session.fireAllRules();
+        }
+    }
--- src/test/java/pl/maciejwalkowiak/drools/AppTest.java
+++ src/test/java/pl/maciejwalkowiak/drools/AppTest.java
@@ -3,36 +3,23 @@ package pl.maciejwalkowiak.drools;
 import org.drools.StatefulSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import pl.maciejwalkowiak.drools.annotations.DroolsSession;
 import pl.maciejwalkowiak.drools.annotations.DroolsFiles;
+import pl.maciejwalkowiak.drools.annotations.DroolsSession;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(DroolsJUnitRunner.class)
 @DroolsFiles(value = "helloworld.drl", location = "/drl/")
 public class AppTest {
 
     @DroolsSession
-    pl.maciejwalkowiak.drools.DroolsSession session;
-
-    @DroolsSession
-    StatefulSession statefulSession;
+    StatefulSession session;
 
     @Test
     public void testApp() {
         session.insert(new App());
-        session.fire("Hello world");
+        session.fireAllRules();
 
-        assertTrue(true);
         assertNotNull(session);
-        assertNotNull(statefulSession);
-        assertEquals(session.getStatefulSession(), statefulSession);
-    }
-
-    @Test
-    public void foo() {
-
     }
 }
